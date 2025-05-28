8dd4d8254619f3caab3cb00709e795904567b5b7
Anna Terekhina <a56s@mail.ru>
2025-05-28 15:46:52 +0300

    sprint7

--- src/test/java/GetOrdersTest.java
+++ src/test/java/GetOrdersTest.java
@@ -23,12 +23,5 @@ public class GetOrdersTest {
                 .then()
                 .statusCode(200)
                 .body("orders", notNullValue());
-
-        Response response = given()
-                .header("Content-type", "application/json")
-                .when()
-                .get("/api/v1/orders")
-                .then()
-                .extract().response();
     }
 }
